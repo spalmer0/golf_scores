01b5f0c930c9c94bcd9ba535b778b24d953d2a87
Andrew Palmer <andrew.palmer.10@gmail.com>
2020-07-12 14:35:39 -0400

    Allow for CSV download

--- app/controllers/golfers_controller.rb
+++ app/controllers/golfers_controller.rb
@@ -1,5 +1,10 @@
 class GolfersController < ApplicationController
   def index
     @golfers = Golfer.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @golfers.to_csv, filename: "GolferData-#{Date.today}.csv" }
+    end
   end
 end
