bdf5090582578ad106f142b08bf08df21116af93
Andrew Palmer <andrew.palmer.10@gmail.com>
2020-07-23 22:15:48 -0400

    Allow user to edit rotogrinders URL

--- app/controllers/data_sources_controller.rb
+++ app/controllers/data_sources_controller.rb
@@ -10,11 +10,36 @@ class DataSourcesController < ApplicationController
   end
 
   def edit
+    @data_source = DataSource.find(params[:id])
   end
 
   def update
+    @data_source = DataSource.find(params[:id])
+
+    if @data_source.update(data_source_params)
+      redirect_to data_sources_path, notice: 'Data source was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  def data_source_params
+    params
+      .require(:data_source)
+      .permit(
+        :data_types,
+        :destination_column_names,
+        :golfer_column_name,
+        :last_fetched,
+        :source_column_names,
+        :source,
+        :stat,
+        :table_location,
+        :url,
+        :year,
+      )
   end
 end
